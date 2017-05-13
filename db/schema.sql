f66e439b1ff2a33d0265ebed6e668f8f50ac6f08
Natasha Malt <natashamalt@calvisitor-10-105-162-188.calvisitor.1918.berkeley.edu>
2017-05-13 11:52:24 -0700

    schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,12 @@
--- SCHEMA :)
-CREATE DATABASE IF NOT EXISTS leash_2_leash_main_db;
-USE leash_2_leash_main_db;
+CREATE DATABASE leash2leash;
 
+USE leash2leash;
 
-CREATE TABLE account_info (
-id INTEGER (100)AUTO_INCREMENT NOT NULL,
+CREATE TABLE accountinfo (
+id INTEGER (100) AUTO_INCREMENT NOT NULL,
 name VARCHAR (100) NOT NULL,
 email VARCHAR (100) NOT NULL,
 location VARCHAR (100) NOT NULL,
-AM_PM VARCHAR (100) NOT NULL,
-Weekend_Weekday VARCHAR (100) NOT NULL,
-dog_type VARCHAR (100) NOT NULL,
-PRIMARY KEY (id)
+PRIMARY KEY (id) 
 );
+

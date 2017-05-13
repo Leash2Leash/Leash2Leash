34e2689bd8d189af18c510115d560659c79c09a4
Natasha Malt <natashamalt@calvisitor-10-105-162-188.calvisitor.1918.berkeley.edu>
2017-05-13 11:38:21 -0700

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,20 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'leash2leash',
+});
+
+connection.connect(function(err){
+	if (err) {
+		console.log('not working');
+		console.log(err);
+    } else {
+    	console.log('working!');
+    	console.log('working on' + connection.threadId);
+    };
+    
+});

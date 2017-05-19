c32e686358d8ef13ca1737a0a5df925265c61d7e
Pablo Ponce <pabloponce@Pablos-MacBook-Pro.local>
2017-05-18 20:29:10 -0700

    adding HTML

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
--- SCHEMA :)
-CREATE DATABASE IF NOT EXISTS leash_2_leash_main_db;
-USE leash_2_leash_main_db;
+
+CREATE DATABASE IF NOT EXISTS leash2leash_db;
+USE leash2leash_db;
 
 
-CREATE TABLE account_info (
+CREATE TABLE IF NOT EXISTS account_info (
 id INTEGER (100)AUTO_INCREMENT NOT NULL,
-name VARCHAR (100) NOT NULL,
-email VARCHAR (100) NOT NULL,
+user_name VARCHAR (100) NOT NULL,
+user_email VARCHAR (100) NOT NULL,
 location VARCHAR (100) NOT NULL,
-AM_PM VARCHAR (100) NOT NULL,
-Weekend_Weekday VARCHAR (100) NOT NULL,
+am_pm VARCHAR (100) NOT NULL,
+weekend_weekday VARCHAR (100) NOT NULL,
 dog_type VARCHAR (100) NOT NULL,
 PRIMARY KEY (id)
 );
